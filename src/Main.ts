e429a05d73b7247b44482e876fcee23d0da58a22
cl3dson <f.cl3dson@gmail.com>
2019-10-25 11:20:46 -0300

    removinf unused vars

--- src/Main.ts
+++ src/Main.ts
@@ -1,12 +1,24 @@
 import * as bodyParser from 'body-parser';
-import { Container } from 'inversify';
-import { interfaces, InversifyExpressServer, TYPE } from 'inversify-express-utils';
+import { InversifyExpressServer } from 'inversify-express-utils';
 
 import './Presentation/HealthCheckController';
-import {ContainerBuilder} from "./Container/ContainerBuilder";
+import { ContainerBuilder } from './Container/ContainerBuilder';
 
 export class Main {
     public static start(): void {
-        const container = ContainerBuilder
+        const container = ContainerBuilder.build();
+        const server = new InversifyExpressServer(container);
+        server.setConfig(app => {
+            // add body parser
+            app.use(
+                bodyParser.urlencoded({
+                    extended: true,
+                }),
+            );
+            app.use(bodyParser.json());
+        });
+
+        const app = server.build();
+        app.listen(3000);
     }
 }
